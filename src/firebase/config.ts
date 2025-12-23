f0b9776f14325ba5f24270994435c59b387a111a
Maina John <requestflow.0@gmail.com>
2025-12-23 11:15:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5396687354-b7d3b",
+  "appId": "1:103774526538:web:0debff93fa774bbc2b1ab3",
+  "apiKey": "AIzaSyCnQL_Z9wDQcj6MBE_nrdrzuiDCBu17xHs",
+  "authDomain": "studio-5396687354-b7d3b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "103774526538"
+};
